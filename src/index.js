8e88298b826a96b1af4caa208ebc0c3300b998b8
Viktor <viktor_biz@ukr.net>
2020-10-19 23:26:41 +0300

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+  var reversN = n.toString().split("").reverse();
+  var result = '';
+
+  for(var i = 0; i < reversN.length; i++){
+    if(reversN[i] !== '-'){
+      result += reversN[i];
+    }
+  }
   
+  return result;
 }
